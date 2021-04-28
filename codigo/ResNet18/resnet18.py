33d1ee403d91f2a2bd5b02a9fe38e022c7ce74a0
Jorge <jorge.hernan@estudiante.uam.es>
2021-04-28 13:31:49 +0100

    act 28-04-2021

--- codigo/ResNet18/resnet18.py
+++
@@ -1,7 +0,0 @@
-import torch #main module
-import torchvision #computer vision dataset module
-from torchvision import datasets,transforms
-from torch import nn #Keras users will now be really happy
-
-import numpy
-import os
