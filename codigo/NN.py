565bc7cd76c61098e72063bf94a397a735b7ccad
Jorge <jorge.hernan@estudiante.uam.es>
2021-04-28 17:55:54 +0100

    primera red neuronal convolucional

--- codigo/NN.py
+++ codigo/NN.py
@@ -55,7 +55,7 @@ class NN():
             _, pred_label = torch.max(score.data, 1)#pasamos de one hot a número
             correct_cnt_epch = (pred_label == target.data).sum()#calculamos el número de etiquetas correctas
             correct_cnt += correct_cnt_epch
-            ave_loss += loss.data[0]#sumamos el resultado de la función de pérdida para mostrar después
+            ave_loss += loss.data#sumamos el resultado de la función de pérdida para mostrar después
             if train:
                 loss.backward()#Calcula los gradientes y los propaga 
                 self.optimizer.step()#adaptamos los pesos con los gradientes propagados
