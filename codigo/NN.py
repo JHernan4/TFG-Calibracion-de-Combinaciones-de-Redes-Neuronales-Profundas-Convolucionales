35ed5e39a9c7fbbacba4ff0d0534c1886f0b0c97
Jorge <jorge.hernan@estudiante.uam.es>
2021-04-28 18:26:23 +0100

    primera red neuronal convolucional

--- codigo/NN.py
+++ codigo/NN.py
@@ -1,3 +1,5 @@
+#primera aproximacion de entrenamiento de redes neuronales con pytorch
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,26 +14,18 @@ CUDA = torch.cuda.is_available()
 class NET(nn.Module):
     def __init__(self):
         super(NET, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.fc1 = nn.Linear(64*12*12, 128)
-        self.fc2 = nn.Linear(128, 10)#capa de salida
+        self.fc1 = nn.Linear(28*28, 256)#capa oculta
+        self.fc2 = nn.Linear(256, 10)#capa de salida
         self.loss_criterion = nn.CrossEntropyLoss()#Función de pérdida
         
     def forward(self, x, target):
-        x = x.view(-1, 1, 28, 28)#transforma las imágenes a tamaño (n, 1, 28, 28)
-        x = F.relu(self.conv1(x))
-        # la salida de conv1 es 32x26x26
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)#El 2 es el stride
-        # la salida de conv2 es 64x24x24, la salida de max_pool es 64x12x12
-        x = x.view(-1, 64*12*12)#transformamos la salida en un tensor de tamaño (n, 9216)
+        x = x.view(-1, 28*28)#transforma las imágenes de tamaño (n, 28, 28) a (n, 784)
         x = F.relu(self.fc1(x))#Función de activación relu en la salida de la capa oculta
         x = F.softmax(self.fc2(x), dim=1)#Función de activación softmax en la salida de la capa oculta
         return x
         
 
 class NN():
-    """docstring for NN"""
     def __init__(self):
         self.model = NET()
         if CUDA:
@@ -112,23 +106,25 @@ class NN():
 
 
 if __name__ == "__main__":
-    torch.manual_seed(123) #fijamos la semilla
+    torch.manual_seed(123) #fijarmos la semilla
     epochs = 10
 
     trans = transforms.Compose([transforms.ToTensor()]) #Transformador para el dataset
-    root="../data"
+    root="/tmp/"
     train_set = dset.MNIST(root=root, train=True, transform=trans, download=True)
     test_set = dset.MNIST(root=root, train=False, transform=trans)
 
     batch_size = 128
     train_loader = torch.utils.data.DataLoader(
-                     dataset=train_set,
-                     batch_size=batch_size,
-                     shuffle=True)
+                    dataset=train_set,
+                    batch_size=batch_size,
+                    shuffle=True,
+                    pin_memory=CUDA)
     test_loader = torch.utils.data.DataLoader(
                     dataset=test_set,
                     batch_size=batch_size,
-                    shuffle=False)
+                    shuffle=False,
+                    pin_memory=CUDA)
 
     print ('Trainning batch number: {}'.format(len(train_loader)))
     print ('Testing batch number: {}'.format(len(test_loader)))
--- codigo/resnet18.py
+++ codigo/resnet18.py
@@ -1,43 +1,65 @@
-import torch
-model = torch.hub.load('pytorch/vision:v0.9.0', 'resnet18', pretrained=True)
-model.eval()
+from resnet18 import *
+import torch #main module
+if not torch.cuda.is_available():
+	print("unable to run on GPU")
+	exit(-1)
+import torchvision #computer vision dataset module
+from torchvision import datasets,transforms
+from torch import nn #Keras users will now be really happy
 
-# Download an example image from the pytorch website
-import urllib
-url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
-try: urllib.URLopener().retrieve(url, filename)
-except: urllib.request.urlretrieve(url, filename)
+import numpy
+import os
 
-# sample execution (requires torchvision)
-from PIL import Image
-from torchvision import transforms
-input_image = Image.open(filename)
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-input_tensor = preprocess(input_image)
-input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+def lr_scheduler(epoch):
+	if epoch < 150:
+		return 0.1
+	elif epoch < 250:
+		return 0.01
+	elif epoch < 350:
+		return 0.001
 
-# move the input and model to GPU for speed if available
-if torch.cuda.is_available():
-    input_batch = input_batch.to('cuda')
-    model.to('cuda')
 
-with torch.no_grad():
-    output = model(input_batch)
-# Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
-print(output[0])
-# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
-probabilities = torch.nn.functional.softmax(output[0], dim=0)
-print(probabilities)
+if __name__ == '__main__':
+    #1. Definimos las transformaciones del dataset (CIFAR10)
 
-# Read the categories
-with open("imagenet_classes.txt", "r") as f:
-    categories = [s.strip() for s in f.readlines()]
-# Show top categories per image
-top5_prob, top5_catid = torch.topk(probabilities, 5)
-for i in range(top5_prob.size(0)):
-    print(categories[top5_catid[i]], top5_prob[i].item())
+    cifar10_transforms_train=transforms.Compose([transforms.RandomCrop(32, padding=4),
+                   transforms.RandomHorizontalFlip(),
+                   transforms.ToTensor(),
+                   transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+    cifar10_transforms_test=transforms.Compose([transforms.ToTensor(),
+                   transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+
+    #2. Creamos dataset de entrenamiento y de validacion
+    workers = (int)(os.popen('nproc').read()) 
+    cifar10_train=datasets.CIFAR10('/tmp/',train=True,download=True,transform=cifar10_transforms_train)
+    cifar10_test=datasets.CIFAR10('/tmp/',train=False,download=False,transform=cifar10_transforms_test)
+
+    #3. Cargamos los datasets
+    train_loader = torch.utils.data.DataLoader(cifar10_train,batch_size=100,shuffle=True,num_workers=workers)
+    test_loader = torch.utils.data.DataLoader(cifar10_test,batch_size=100,shuffle=False,num_workers=workers)
+
+    #4. Creamos el modelo (resnet18)
+    nn = resnet18()
+
+    scheduler=lr_scheduler
+    for e in range(10):
+        optimizer=torch.optim.SGD(nn.parameters(),lr=scheduler(e),momentum=0.9)
+        for x,t in train_loader: #sample one batch
+		    x,t=x.cuda(),t.cuda()
+		    o=nn.forward_train(x) 
+		    cost=nn.Loss(o,t) 
+		    cost.backward() 
+		    optimizer.step()
+		    optimizer.zero_grad()
+		    ce+=cost.data
+        
+        with torch.no_grad():
+		    for x,t in test_loader:
+			    x,t=x.cuda(),t.cuda()
+			    test_pred=myNet.forward_test(x)
+			    index=torch.argmax(test_pred,1) #compute maximum
+			    MC+=(index!=t).sum().float() #accumulate MC error
+        
+        print("Epoch {} cross entropy {:.5f} and Test error {:.3f}".format(e,ce/500.,100*MC/10000.))
