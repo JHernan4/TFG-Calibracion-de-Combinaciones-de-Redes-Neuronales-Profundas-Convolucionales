0ed0e68f787dc6f6b6e8f61cd2d3f939f9b62ce8
JHernan4 <jorge.hernan@estudiante.uam.es>
2021-05-04 16:53:17 +0200

    optimizando resnet18

--- codigo/resnet18P.py
+++ codigo/resnet18P.py
@@ -5,7 +5,7 @@ if not torch.cuda.is_available():
 import torchvision #computer vision dataset module
 import torchvision.models as models
 from torchvision import datasets,transforms
-from torch import nn
+import torch.nn as nn
 
 import numpy as np
 import os
@@ -25,13 +25,14 @@ if __name__ == '__main__':
 	np.random.seed(123)
 
 	#creacion de las transformaciones que aplicaremos sobre el dataset cifar10
+	print('==> Preparing data...')
 	cifar10_transforms_train=transforms.Compose([transforms.RandomCrop(32, padding=4),
-					   transforms.RandomHorizontalFlip(),
-					   transforms.ToTensor(),
-					   transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]) #transforms are different for train and test
+	                   transforms.RandomHorizontalFlip(),
+	                   transforms.ToTensor(),
+	                   transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]) #transforms are different for train and test
 
 	cifar10_transforms_test=transforms.Compose([transforms.ToTensor(),
-					   transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+	                   transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
 
 	#cargamos el dataset CIFAR10
@@ -40,23 +41,17 @@ if __name__ == '__main__':
 	cifar10_test=datasets.CIFAR10('/tmp/',train=False,download=False,transform=cifar10_transforms_test)
 
 	#creamos los dataloaders para iterar el conjunto de datos
-	train_loader = torch.utils.data.DataLoader(cifar10_train,batch_size=100,shuffle=True,num_workers=workers)
-	test_loader = torch.utils.data.DataLoader(cifar10_test,batch_size=100,shuffle=False,num_workers=workers)
+	train_loader = torch.utils.data.DataLoader(cifar10_train,batch_size=128,shuffle=True,num_workers=workers)
+	test_loader = torch.utils.data.DataLoader(cifar10_test,batch_size=128,shuffle=False,num_workers=workers)
 
+    print('==> Building model..')
+    resnet18 = models.resnet18(False)
 	loss = nn.CrossEntropyLoss()
-	resnet18 = models.resnet18(False)
-	resnet18.SoftMax = nn.Softmax()
-	resnet18.cuda()
 	scheduler=lr_scheduler
-	print(resnet18)
-	print("OK")
-
 	for e in range(350):
 		ce_test,MC,ce=[0.0]*3
-		#now create and optimizer
-		optimizer=torch.optim.SGD(resnet18.parameters(),lr=scheduler(e),momentum=0.9)
-
-		for x,t in train_loader: #sample one batch
+		optimizer=torch.optim.adam(resnet18.parameters(),lr=scheduler(e),momentum=0.9)
+		for x,t in train_loader:
 			x,t=x.cuda(),t.cuda()
 			resnet18.train()
 			o=resnet18.forward(x)
@@ -69,9 +64,9 @@ if __name__ == '__main__':
 		with torch.no_grad():
 			for x,t in test_loader:
 				x,t=x.cuda(),t.cuda()
-				resnet18.eval()
+				resnet18.test()
 				test_pred=resnet18.forward(x)
 				index=torch.argmax(test_pred,1) #compute maximum
 				MC+=(index!=t).sum().float() #accumulate MC error
 
-	print("Epoch {} cross entropy {:.5f} and Test error {:.3f}".format(e,ce/500.,100*MC/10000.))
+		print("Epoch {} cross entropy {:.5f} and Test error {:.3f}".format(e,ce/500.,100*MC/10000.))
