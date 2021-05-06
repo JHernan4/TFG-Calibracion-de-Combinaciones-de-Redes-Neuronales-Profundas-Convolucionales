c1c6f1b52f514d8773455f2f61fddb20d701dcd0
JHernan4 <jorge.hernan@estudiante.uam.es>
2021-05-06 17:31:58 +0200

    procesamiento paralelo

--- codigo/resnet18Par.py
+++ codigo/resnet18Par.py
@@ -31,8 +31,27 @@ if __name__ == '__main__':
     nModelos = 5
     scheduler = lr_scheduler
     print("==> Preparing data...")
+    #creacion de las transformaciones que aplicaremos sobre el dataset cifar10
+	cifar10_transforms_train=transforms.Compose([transforms.RandomCrop(32, padding=4),
+	                   transforms.RandomHorizontalFlip(),
+	                   transforms.ToTensor(),
+	                   transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]) #transforms are different for train and test
+
+	cifar10_transforms_test=transforms.Compose([transforms.ToTensor(),
+	                   transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+
+	#cargamos el dataset CIFAR10
+	workers = (int)(os.popen('nproc').read())
+	cifar10_train=datasets.CIFAR10('/tmp/',train=True,download=True,transform=cifar10_transforms_train)
+	cifar10_test=datasets.CIFAR10('/tmp/',train=False,download=False,transform=cifar10_transforms_test)
+    #creamos los dataloaders para iterar el conjunto de datos
+    train_loader = torch.utils.data.DataLoader(cifar10_train,batch_size=100,shuffle=True,num_workers=workers, worker_init_fn=seed_worker)
+    test_loader = torch.utils.data.DataLoader(cifar10_test,batch_size=100,shuffle=False,num_workers=workers, worker_init_fn=seed_worker)
 
     model = ResNet18()
-    net = torch.nn.DataParalell(model, device_ids=[0,1])
-    output = net(0.65)
-    print(output)
+    net = torch.nn.DataParallel(model, device_ids=[0,1])
+    for x,t in train_loader:
+        x,t=x.cuda(),t.cuda()
+        output = net(x)
+        print(output)
