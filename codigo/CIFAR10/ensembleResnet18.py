9a009102cea03aae5c96b830fba102d1462a3c77
JHernan4 <jorge.hernan@estudiante.uam.es>
2021-05-15 11:45:07 +0200

    creando ensemble resnet18

--- codigo/CIFAR10/ensembleResnet18.py
+++ codigo/CIFAR10/ensembleResnet18.py
@@ -1,4 +1,3 @@
-from codigo.CIFAR10.resnet18 import PATH
 import torch
 if not torch.cuda.is_available():
     print("Error al cargar GPU")
@@ -16,20 +15,45 @@ import os
 import random
 import argparse
 
-
 def parse_args():
     parser = argparse.ArgumentParser(description='Parametros para configuracion del entrenamiento de las redes neuronales convolucionales')
     parser.add_argument('--nModelos', help="número de modelos que componen el emsemble", required=True, type = int)
     args = parser.parse_args()
     return args
 
+def seed_worker(worker_id):
+    worker_seed=torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+def explotation(self, testLoader, n):
+    with torch.no_grad():
+        correct,total=0,0
+        for x,t in test_loader:
+            x,t=x.cuda(),t.cuda()
+            test_pred=self.forward(x)
+            index=torch.argmax(test_pred,1)
+            total+=t.size(0)
+            correct+=(index==t).sum().float()
+    print("Modelo {}: accuracy {}".format(n+1, 100*correct/total))
 
 if __name__ == '__main__':
-    PATH = './checkpoint'+'_resnet18'
     args = parse_args()
+    PATH = './checkpoint'+'_resnet18'
     nModelos = args.nModelos
+
+    workers = (int)(os.popen('nproc').read())
+    cifar10_transforms_test=transforms.Compose([transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+    
+    cifar10_test=datasets.CIFAR10('/tmp/',train=False,download=False,transform=cifar10_transforms_test)
+    test_loader = torch.utils.data.DataLoader(cifar10_test,batch_size=100,shuffle=False,num_workers=workers, worker_init_fn=seed_worker)
+
+
     for n in range(nModelos):
         model = ResNet18()
-        PATH = PATH + "_"+str(n+1) + '.pt'
-        model.load_state_dict(torch.load(PATH))
+        model = torch.nn.DataParallel(model, device_ids=[0,1]).cuda()
+        model.load_state_dict(torch.load(PATH+"_"+str(n+1) + '.pt'))
         print("Modelo {} cargado correctamente".format(n+1))
+        model.eval()
+        model.explotation(test_loader, n)
