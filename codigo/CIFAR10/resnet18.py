0d1be73a94642eadbd9d41ac036a2f843d7e9995
JHernan4 <jorge.hernan@estudiante.uam.es>
2021-06-15 09:16:32 +0200

    r18 y r50

--- codigo/CIFAR10/resnet18.py
+++ codigo/CIFAR10/resnet18.py
@@ -4,156 +4,218 @@
 #para ser posteriormente cargado por el programa ensembleResnet18.py 
 ##################################################################################################################
 
+import warnings
 import torch
-if not torch.cuda.is_available():
-    print("Error al cargar GPU")
-    exit(-1)
-import torchvision #computer vision dataset module
-import torchvision.models as models
-from torchvision import datasets,transforms
-from torch import nn
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+import os
+import argparse
 import sys
+import matplotlib.pyplot as plt
+from torchsummary import summary
+import numpy as np
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+#total_epoch = 100
+
+'''path setting'''
+parser = argparse.ArgumentParser(description='Train and Test UPANets with PyTorch')
+parser.add_argument('--pkg_path', default='./', type=str, help='package path')
+parser.add_argument('--save_path', default='./resultsResnet18/', type=str, help='package path')
+
+'''experiment setting'''
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--datasets', default='cifar_10', type=str, help='using dataset')
+parser.add_argument('--batch_size', default=100, type=int, help='batch size')
+parser.add_argument('--epochs', default=250, type=int, help='total traing epochs')
+
+'''model setting'''
+parser.add_argument('--blocks', default=1, type=int, help='block number in UPANets')
+parser.add_argument('--filters', default=16, type=int, help='filter number in UPANets')
+
+args = parser.parse_args()
+
+pkgpath = args.pkg_path
+save_path = args.save_path
+
+if os.path.isdir(save_path) == False:
+    os.makedirs(save_path)
+
+sys.path.append(pkgpath)
+
 sys.path.append("../models")
 from resnet import ResNet18
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import random
-import argparse
 
-def lr_scheduler(epoch):
-    if epoch < 150:
-        return 0.1
-    elif epoch < 250:
-        return 0.01
-    elif epoch < 350:
-        return 0.001
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-scheduler = lr_scheduler
-
-class MyModel():
-
-    def __init__(self, model, nEpocas=250):
-        self.net = model
-        self.trainAccuracies = np.empty(nEpocas)
-        self.validationAccuracies = np.empty(nEpocas)
-
-    def trainModel(self, trainLoader, validationLoader, seed, nModelo, path, nEpocas=250):
-        loss = nn.CrossEntropyLoss()
-        sm = nn.Softmax(dim=1)
-        torch.manual_seed(seed)
+# Data
+print('==> Preparing data..')
+img_size = 32 # default image size for Cifar-10
+if args.datasets == 'cifar_10' or args.datasets == 'cifar_100':
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+    
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+    
+    if args.datasets == 'cifar_10':
+        trainset = torchvision.datasets.CIFAR10(
+            root='/tmp/', train=True, download=True, transform=transform_train)
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=args.batch_size, shuffle=True, num_workers=0)
         
-        path = path + "_"+str(nModelo+1) + '.pt'
-        for e in range(nEpocas):
-            print("Epoca {}/{}".format(e+1, nEpocas))
-            correctT, totalT, correctV, totalV = 0,0,0,0
-            ceT, ceV = 0,0
-            optimizer=torch.optim.SGD(model.parameters(),lr=scheduler(e),momentum=0.9)
-            self.net.train()
-            counter=0
-            for x,t in trainLoader:
-                x,t=x.cuda(),t.cuda()
-                pred=self.net.forward(x)
-                cost=loss(pred,t)
-                cost.backward()
-                optimizer.step()
-                optimizer.zero_grad()
-                ceT+=cost.data
-                counter+=1
+        testset = torchvision.datasets.CIFAR10(
+            root='/tmp/', train=False, download=True, transform=transform_test)
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=args.batch_size, shuffle=False, num_workers=0)
+        classes = 10
+        img_size = 32
+        
+    if args.datasets == 'cifar_100':
+        trainset = torchvision.datasets.CIFAR100(
+            root='/tmp/', train=True, download=True, transform=transform_train)
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=args.batch_size, shuffle=True, num_workers=0)
+        
+        testset = torchvision.datasets.CIFAR100(
+            root='/tmp/', train=False, download=True, transform=transform_test)
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=args.batch_size, shuffle=False, num_workers=0)
+        classes = 100
+        img_size = 32
+        
 
-                with torch.no_grad():
-                    pred = sm(pred)
-                    index = torch.argmax(pred, 1)
-                    totalT+=t.size(0)
-                    correctT+=(t==index).sum().float()
+# Model
+
+#%%
+# Training
+def train(net, optimizer, epoch):
+    print('Epoch:{0}/{1}'.format(epoch, args.epochs))
+    net.train()
+    
+    train_loss = 0 
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)        
+        loss = criterion(outputs, targets)       
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+    return train_loss/(batch_idx+1), 100.*correct/total
+
+def test(net, epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    acc_list = []
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+            acc_list.append(100.*correct/total)
             
-            print("\tTrain accuracy: {}".format(ceT/counter))
-            self.trainAccuracies[e] = ceT/counter
-            counter=0
-            for x,t in validationLoader:
-                with torch.no_grad():
-                    x,t=x.cuda(),t.cuda()
-                    pred=self.net.forward(x)
-                    costV = loss(pred,t)
-                    pred = sm(pred)
-                    index = torch.argmax(pred, 1)
-                    totalV+=t.size(0)
-                    correctV+=(t==index).sum().float()
-                    ceV+=costV.data
-                    counter+=1
-            
-            print("\tValidation loss: {}".format(ceV/counter))
-            self.validationAccuracies[e] = ceV/counter
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        best_acc = acc
+
+    print('>>>best acc:', best_acc)
+    
+    return test_loss/(batch_idx+1), 100.*correct/total, best_acc
+
+PATH = './checkpointResnet50/checkpoint'+'_upanets'
+np.random.seed(123)
+for n in range(5):
+    test_loss = 0
+    test_list = []
+    train_list = []
+    epoch_list = []
+    train_acc_list = []
+    test_acc_list = []
+    best_acc = 0
+    seed = np.random.randint(2**10)
+    torch.manual_seed(seed)
+    if n <= 1:
+        break
+    print("Modelo {}".format(n+1))
+    print('==> Building model..')
+    net =  ResNet18(num_classes=classes)
+    net = net.to(device)
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.1,
+                      momentum=0.9, weight_decay=0.0005)
+
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs)
+    for epoch in range(start_epoch, start_epoch+args.epochs):
+        epoch_list.append(epoch)
+    
+        train_loss, train_acc = train(net, optimizer, epoch)
+        train_list.append(train_loss)
+        train_acc_list.append(train_acc)
+    
+        test_loss, test_acc, best_acc = test(net, epoch)
+        test_list.append(test_loss)
+        test_acc_list.append(test_acc)
+    
+        epoch_line = 'epoch: {0}/ total epoch: {1} '.format(epoch, args.epochs) 
+        best_acc_line = 'best_acc: {0} '.format(best_acc)
+        accuracy_line = 'train_acc: {0} %, test_acc: {1} % '.format(train_acc, test_acc)
+        loss_line = 'train_loss: {0},e test_loss: {1} '.format(train_loss, test_loss)
+
+        with open(save_path+'/logs.txt', 'a') as f:
+            f.write(epoch_line + best_acc_line + accuracy_line + loss_line + '\n')
+        scheduler.step()
+    
+    plt.subplot(2, 1, 1)
+    plt.plot(epoch_list, train_list, c = 'blue', label = 'train loss')
+    plt.plot(epoch_list, test_list, c = 'red', label = 'test loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(loc=0)
         
-        torch.save(self.net.state_dict(), path)
-        print("Modelo {} guardado correctamente en {}".format(nModelo+1, path))	
-        return self.net
-
-    def saveGraficas(self, nModelo, nEpocas=250):
-        file = 'checkpointResnet18Tra/resnet18_'+str(nModelo+1)+'.jpg'
-        x = np.linspace(0,nEpocas,nEpocas)
-        plt.figure()
-        plt.plot(x, self.trainAccuracies, label="Train")
-        plt.plot(x, self.validationAccuracies, label="Validation")
+    plt.subplot(2, 1, 2)
+    plt.plot(epoch_list, train_acc_list, c = 'blue', label = 'train acc')
+    plt.plot(epoch_list, test_acc_list, c = 'red', label = 'test acc')
+    plt.ylabel('acc')
+    plt.xlabel('epoch')
+    plt.legend(loc=0)
         
-        plt.xlabel("Number of epoch")
-        plt.ylabel("Loss")
-        plt.title("Loss Resnet18 on CIFAR10")
-        plt.legend()
-        plt.savefig(file)
- 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Parametros para configuracion del entrenamiento de las redes neuronales convolucionales')
-    parser.add_argument('--seed', help='semilla para inicializar generador de numeros aleatorios de numpy', required=True, type=int)
-    parser.add_argument('--nEpocas', help='número de epocas para el entrenamiento de las redes neuronales', required=True, type=int)
-    parser.add_argument('--nModelos', help="número de modelos que componen el emsemble", required=True, type = int)
-    args = parser.parse_args()
-    return args
+    plt.savefig(save_path+'/resnet18_train_history_'+str(n+1)+'.png')
+    plt.clf()
+    print("Gráfica guardada correctamente")
 
-def separarConjunto(dataset, trainSize=10000):
-    val_set, train_set = torch.utils.data.random_split(dataset, [len(dataset)-trainSize, trainSize])
-    print(len(val_set))
-    print(len(train_set))
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=100, shuffle=False, num_workers=workers)
-    val_loader = torch.utils.data.DataLoader(val_set, batch_size=100, shuffle=False, num_workers=workers)
-
-    return train_loader, val_loader
-
-
-def seed_worker(worker_id):
-    worker_seed=torch.initial_seed() % 2**32
-    np.random.seed(worker_seed)
-    random.seed(worker_seed)
-
-
-
-if __name__ == '__main__':
-    PATH = './checkpointResnet18Tra/checkpoint'+'_resnet18'
-    args = parse_args()
-    nModelos = args.nModelos
-    nEpocas = args.nEpocas
-    
-    
-    print("==> Preparando dataset de entrenamiento...")
-    cifar10_transforms_train=transforms.Compose([transforms.RandomCrop(32, padding=4),
-                        transforms.RandomHorizontalFlip(),
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
-
-
-    workers = (int)(os.popen('nproc').read())
-    cifar10_train=datasets.CIFAR10('/tmp/',train=True,download=True,transform=cifar10_transforms_train)
-    
-       
-    train_loader, validation_loader = separarConjunto(cifar10_train)
-    
-    print("==> Entrenando modelos...")
-    models=[]
-    for n in range(nModelos):
-        model=ResNet18()
-        model = torch.nn.DataParallel(model, device_ids=[0,1]).cuda()
-        net = MyModel(model, nEpocas)
-        seed = np.random.randint(2**10)
-        models.append(net.trainModel(train_loader, validation_loader, seed, n, PATH, nEpocas))
-
-        net.saveGraficas(n, nEpocas)
+    torch.save(net.state_dict(), PATH+"_"+str(n+1) + '.pt')
+    print("Modelo {} guardado correctamente.".format(n+1))
